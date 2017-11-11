8263bd141d6f95bc11749af8101b14cb91d2feae
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2017-11-11 16:43:13 +0900

    20171111 16:43

--- set=.lisp
+++ set=.lisp
@@ -2,6 +2,13 @@
 ; 1. Everyone can use, copy and modify the following code.
 ; 2. Everyone can erase this copyright notice and licence terms.
 
+; [examples]
+; (set= (+ a 1)) => (setq a (+ a 1))
+; (set= (/ b 2)) => (setq b (/ b 2))
+; (set= (cons 3 c)) => (setq c (cons 3 c))
+; (set= (cddr lst1) (cdr lst2))
+;   => (setq lst1 (cddr lst1)) (setq lst2 (cdr lst2))
+
 (defmacro set= (&rest proc-var-form-list)
   (let ((lst))
     (dolist (pvf proc-var-form-list)
