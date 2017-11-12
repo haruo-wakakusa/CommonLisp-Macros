65608d6c3ffa8fe0b98be4aa9387c748261031fe
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2017-11-12 12:17:04 +0900

    20171112 12:17

--- string-join.lisp
+++ string-join.lisp
@@ -2,6 +2,10 @@
 ; 1. Everyone can use, copy and modify the following code.
 ; 2. Everyone can erase this copyright notice and licence terms.
 
+; (thanks to Shiro Kawai)
+; you can also write like this:
+; (format nil "~{~a~^, ~}" (list "A" "B" "C")) => "A, B, C"
+
 (defun string-join (separator string-list)
   (let ((s (make-string-output-stream)) (res))
     (write-string (car string-list) s)
