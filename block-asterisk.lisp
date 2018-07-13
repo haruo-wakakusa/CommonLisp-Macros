7dc6c0b5577cb7a79617946e06981fe6485590e3
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2018-07-13 16:55:01 +0900

    20180713 16:54

--- block-asterisk.lisp
+++ block-asterisk.lisp
@@ -1,4 +1,4 @@
-; Copyright 2017 Haruo Wakakusa (haruowakakusa@gmail.com)
+; Copyright 2018 Haruo Wakakusa (haruowakakusa@gmail.com)
 ; 1. Everyone can use, copy and modify the following code.
 ; 2. Everyone can erase this copyright notice and licence terms.
 
